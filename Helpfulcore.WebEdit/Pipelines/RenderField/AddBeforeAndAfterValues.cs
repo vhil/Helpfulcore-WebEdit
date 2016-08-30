59308311b80a6d78f397dfa05c93ebcb02358263
Volodymyr HIl <vhi@cabana.dk>
2016-08-30 21:09:30 +0200

    before, after and enclosing tag

---
+++ Helpfulcore.WebEdit/Pipelines/RenderField/AddBeforeAndAfterValues.cs
@@ -0,0 +1,31 @@
+﻿using Sitecore.Pipelines.RenderField;
+
+namespace Helpfulcore.WebEdit.Pipelines.RenderField
+{
+	public class AddBeforeAndAfterValues : Sitecore.Pipelines.RenderField.AddBeforeAndAfterValues
+	{
+		protected const string BeforeKey = "Before";
+		protected const string AfterKey = "After";
+		protected const string EnclosingTagKey = "EnclosingTag";
+
+		public new void Process(RenderFieldArgs args)
+		{
+			args.Before = this.ExtractParameter(args, "Before");
+			args.After = this.ExtractParameter(args, "After");
+			args.EnclosingTag = this.ExtractParameter(args, "EnclosingTag");
+		}
+
+		private string ExtractParameter(RenderFieldArgs args, string key)
+		{
+			var parameter = string.Empty;
+
+			if (args.Parameters.ContainsKey(key))
+			{
+				parameter = args.Parameters[key];
+				args.Parameters.Remove(key);
+			}
+
+			return parameter;
+		}
+	}
+}
---
+++ Helpfulcore.WebEdit/Pipelines/RenderField/RenderWebEditing.cs
@@ -0,0 +1,51 @@
+﻿using Sitecore;
+using Sitecore.Pipelines.RenderField;
+
+namespace Helpfulcore.WebEdit.Pipelines.RenderField
+{
+	public class RenderWebEditing : Sitecore.Pipelines.RenderField.RenderWebEditing
+	{
+		public new void Process(RenderFieldArgs args)
+		{
+			var before = args.Before;
+			var after = args.After;
+			var enclosingTag = args.EnclosingTag;
+
+			args.Before = string.Empty;
+			args.After = string.Empty;
+			args.EnclosingTag = string.Empty;
+
+			base.Process(args);
+
+			if (this.IsWrappingEnabled(args))
+			{
+				if (!string.IsNullOrWhiteSpace(enclosingTag))
+				{
+					args.Result.FirstPart = string.Format("<{0}>{1}", enclosingTag, args.Result.FirstPart);
+				}
+
+				if (!string.IsNullOrWhiteSpace(before))
+				{
+					args.Result.FirstPart = before + args.Result.FirstPart;
+				}
+
+				if (!string.IsNullOrWhiteSpace(enclosingTag))
+				{
+					args.Result.LastPart = string.Format("{0}</{1}>", args.Result.LastPart, enclosingTag);
+				}
+
+				if (!string.IsNullOrWhiteSpace(after))
+				{
+					args.Result.LastPart = args.Result.LastPart + after;
+				}
+			}
+		}
+
+		protected bool IsWrappingEnabled(RenderFieldArgs args)
+		{
+			return !string.IsNullOrWhiteSpace(args.FieldValue)
+				   || Context.PageMode.IsPageEditor
+				   || Context.PageMode.IsPageEditorEditing;
+		}
+	}
+}
